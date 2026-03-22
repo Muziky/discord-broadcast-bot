1e3c42f5323e6efcc8813dd5ab0cc6568b1fe48e
Muziky <45007174+Muziky@users.noreply.github.com>
2026-03-22 12:11:33 +0300

    Refactor config to use environment variables
    Updated configuration to use environment variables for sensitive data.

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,15 @@
 import { WebhookClient } from "discord.js";
 
 export default {
-    token: "", // Bot token
+    token: process.env.token, // Bot token
     color: "903EB8", // Default color for embeds
     prefix: "-", // Bot prefix its not used in the bot
-    WebhookUrl: new WebhookClient({ url: "" }), // Webhook URL for image uploads
+    WebhookUrl: new WebhookClient({ url: process.env.webhook }), // Webhook URL for image uploads
     debugMode: false, // Debug mode for the bot this allow only developers to use the bot
-    developers: ["622486784038666242", "527826654660132890", "251701185164214272","647488382930518028","903340403430944808",], // Developer IDs for the bot
-    whiteListedGuilds: ["738862529081507870", "1299331348913328148", "1042879508493127721"], // Whitelisted guilds for the bot to work in
+    developers: [`${process.env.dev}`], // Developer IDs for the bot
+    whiteListedGuilds: [`${process.env.server}`], // Whitelisted guilds for the bot to work in
     host: "https://7xrr.glitch.me",
     editSpeed: 10,
-    text: ["This bot was custom-developed specifically for the London S Discord server"],
+    text: ["This bot was custom-developed"],
     customPassword: '72717xrhazeZ$'
 };
