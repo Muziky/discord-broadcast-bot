65fcdae6884c9293ee27a5895ae38d1966d8d19c
Muziky <45007174+Muziky@users.noreply.github.com>
2026-03-22 15:28:42 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { WebhookClient } from "discord.js";
 export default {
     token: process.env.token, // Bot token
     color: "903EB8", // Default color for embeds
-    prefix: "-", // Bot prefix its not used in the bot
+    prefix: process.env.prefix || "-", // Bot prefix its not used in the bot
     WebhookUrl: new WebhookClient({ url: process.env.webhook }), // Webhook URL for image uploads
     debugMode: false, // Debug mode for the bot this allow only developers to use the bot
     developers: [`${process.env.dev}`], // Developer IDs for the bot
